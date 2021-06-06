6a20d7ae8bedc6fb94ccd9434608c3b14085719c
Dustin Brody <tersec@users.noreply.github.com>
2021-06-06 10:26:21 +0200

    use ForkedHashedBeaconState in StateData

--- beacon_chain/gossip_processing/gossip_validation.nim
+++ beacon_chain/gossip_processing/gossip_validation.nim
@@ -16,7 +16,8 @@ import
   # Internals
   ../spec/[
     beaconstate, state_transition_block,
-    datatypes, crypto, digest, helpers, network, signatures],
+    datatypes, crypto, digest, forkedbeaconstate_helpers, helpers, network,
+    signatures],
   ../consensus_object_pools/[
     spec_cache, blockchain_dag, block_quarantine, spec_cache,
     attestation_pool, exit_pool
@@ -249,9 +250,9 @@ proc validateAttestation*(
       "validateAttestation: number of aggregation bits and committee size mismatch")))
 
   let
-    fork = getStateField(pool.dag.headState, fork)
+    fork = getStateField(pool.dag.headState.data, fork)
     genesis_validators_root =
-      getStateField(pool.dag.headState, genesis_validators_root)
+      getStateField(pool.dag.headState.data, genesis_validators_root)
     attesting_index = get_attesting_indices_one(
       epochRef, attestation.data, attestation.aggregation_bits)
 
@@ -431,9 +432,9 @@ proc validateAggregate*(
       return err((ValidationResult.Reject, cstring("Invalid aggregator_index")))
 
   let
-    fork = getStateField(pool.dag.headState, fork)
+    fork = getStateField(pool.dag.headState.data, fork)
     genesis_validators_root =
-      getStateField(pool.dag.headState, genesis_validators_root)
+      getStateField(pool.dag.headState.data, genesis_validators_root)
 
   let deferredCrypto = batchCrypto
                 .scheduleAggregateChecks(
@@ -596,7 +597,8 @@ proc isValidBeaconBlock*(
   # compute_start_slot_at_epoch(store.finalized_checkpoint.epoch)) ==
   # store.finalized_checkpoint.root
   let
-    finalized_checkpoint = getStateField(dag.headState, finalized_checkpoint)
+    finalized_checkpoint = getStateField(
+      dag.headState.data, finalized_checkpoint)
     ancestor = get_ancestor(
       parent_ref, compute_start_slot_at_epoch(finalized_checkpoint.epoch))
 
@@ -629,8 +631,8 @@ proc isValidBeaconBlock*(
   # [REJECT] The proposer signature, signed_beacon_block.signature, is valid
   # with respect to the proposer_index pubkey.
   if not verify_block_signature(
-      getStateField(dag.headState, fork),
-      getStateField(dag.headState, genesis_validators_root),
+      getStateField(dag.headState.data, fork),
+      getStateField(dag.headState.data, genesis_validators_root),
       signed_beacon_block.message.slot,
       signed_beacon_block.message,
       dag.validatorKeys[proposer.get()],
@@ -670,7 +672,7 @@ proc validateAttesterSlashing*(
   # validation.
   let attester_slashing_validity =
     check_attester_slashing(
-      pool.dag.headState.data.data, attester_slashing, {})
+      pool.dag.headState.data.hbsPhase0.data, attester_slashing, {})
   if attester_slashing_validity.isErr:
     return err((ValidationResult.Reject, attester_slashing_validity.error))
 
@@ -700,7 +702,7 @@ proc validateProposerSlashing*(
   # [REJECT] All of the conditions within process_proposer_slashing pass validation.
   let proposer_slashing_validity =
     check_proposer_slashing(
-      pool.dag.headState.data.data, proposer_slashing, {})
+      pool.dag.headState.data.hbsPhase0.data, proposer_slashing, {})
   if proposer_slashing_validity.isErr:
     return err((ValidationResult.Reject, proposer_slashing_validity.error))
 
@@ -718,7 +720,7 @@ proc validateVoluntaryExit*(
   # [IGNORE] The voluntary exit is the first valid voluntary exit received for
   # the validator with index signed_voluntary_exit.message.validator_index.
   if signed_voluntary_exit.message.validator_index >=
-      getStateField(pool.dag.headState, validators).lenu64:
+      getStateField(pool.dag.headState.data, validators).lenu64:
     return err((ValidationResult.Ignore, cstring(
       "validateVoluntaryExit: validator index too high")))
 
@@ -734,7 +736,7 @@ proc validateVoluntaryExit*(
   # validation.
   let voluntary_exit_validity =
     check_voluntary_exit(
-      pool.dag.headState.data.data, signed_voluntary_exit, {})
+      pool.dag.headState.data.hbsPhase0.data, signed_voluntary_exit, {})
   if voluntary_exit_validity.isErr:
     return err((ValidationResult.Reject, voluntary_exit_validity.error))
 
--- beacon_chain/spec/signatures_batch.nim
+++ beacon_chain/spec/signatures_batch.nim
@@ -13,7 +13,9 @@ import
   stew/[byteutils, results],
   # Internal
   ../ssz/merkleization,
-  "."/[crypto, datatypes, helpers, presets, beaconstate, digest]
+  "."/[
+    crypto, datatypes, helpers, presets, beaconstate, digest,
+    forkedbeaconstate_helpers]
 
 # Otherwise, error.
 import chronicles
@@ -94,7 +96,7 @@ proc addIndexedAttestation(
       sigs: var seq[SignatureSet],
       attestation: IndexedAttestation,
       validatorKeys: openArray[CookedPubKey],
-      state: StateData,
+      state: ForkedHashedBeaconState,
      ): Result[void, cstring] =
   ## Add an indexed attestation for batched BLS verification
   ## purposes
@@ -119,12 +121,13 @@ proc addAttestation(
       sigs: var seq[SignatureSet],
       attestation: Attestation,
       validatorKeys: openArray[CookedPubKey],
-      state: StateData,
+      state: ForkedHashedBeaconState,
       cache: var StateCache
      ): Result[void, cstring] =
+
   var inited = false
   var attestersAgg{.noInit.}: AggregatePublicKey
-  for valIndex in state.data.data.get_attesting_indices(
+  for valIndex in state.get_attesting_indices(
                     attestation.data,
                     attestation.aggregation_bits,
                     cache
@@ -246,7 +249,7 @@ proc collectSignatureSets*(
        sigs: var seq[SignatureSet],
        signed_block: SignedBeaconBlock,
        validatorKeys: openArray[CookedPubKey],
-       state: StateData,
+       state: ForkedHashedBeaconState,
        cache: var StateCache): Result[void, cstring] =
   ## Collect all signatures in a single signed block.
   ## This includes
@@ -377,6 +380,7 @@ proc collectSignatureSets*(
     ? sigs.addAttestation(
         signed_block.message.body.attestations[i],
         validatorKeys, state, cache)
+
 
   # 6. VoluntaryExits
   # ----------------------------------------------------
